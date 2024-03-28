3a77c40b0a46cb1a8aeb4b1edb6ad2fce22deb9d
Davide Carraro <124809373+Daveeeee13@users.noreply.github.com>
2024-03-28 14:37:06 +0100

    si

---
+++ php/logout.php
@@ -0,0 +1,10 @@
+<?php
+	session_start();
+	// Elimino il token di autenticazione dalla sessione, in modo checkdate
+	// fallista il controllo per l'accesso all'area riservata
+	// vedi: if (!isset($_SESSION["AUTENTICATO"])) (home_riservata.php)
+	// https://www.php.net/manual/en/function.unset.php
+	// https://www.w3schools.com/php/func_var_unset.asp
+	unset($_SESSION["AUTENTICATO"]);
+	header("Location: ../index.html");
+?>
