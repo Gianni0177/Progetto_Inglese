c744f3df726e8531b095f9927fea88e783bd1fb6
Davide Carraro <124809373+Daveeeee13@users.noreply.github.com>
2024-04-04 16:30:41 +0200

    Adding account details page

--- areariservata.php
+++ areariservata.php
@@ -1,3 +1,10 @@
+<?php 
+session_start();
+if(!$_SESSION["AUTENTICATO"]=="ok"){
+    header("Location: php/login.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,12 +26,13 @@
   <br><br><br><br><br>
   <!-- Per aggiungere righe copia incolla un li-->
   <ul>
-    <li><a href="graduation.html"><i class='bx bxs-graduation'></i> graduation</a></li>
-    <li><a href="index.php"><i class='bx bx-home-alt-2' ></i> return to home</a></li>
-    <li><a href="php/logout.php"><i class='bx bxs-log-out'></i> sign-out</a></li>
+    <!--<li><a href="graduation.html"><i class='bx bxs-graduation'></i> graduation</a></li>-->
+    <li><a href="index.php"><i class='bx bx-home'></i> HomePage</a></li>
+    <li><a href="php/sign-in.php"><i class='bx bxs-log-in'></i> sign in</a></li>
     <li><a href="termsOfUse.html"><i class='bx bx-user'></i> terms of use</a></li>
-    <!-- <li><a href="settings.html"><i class='bx bx-cog'></i> settings</a></li> -->
-    </ul>
+    <li><a href="credits.html"><i class='bx bxl-creative-commons'></i> credit</a></li>
+  <!-- <li><a href="settings.html"><i class='bx bx-cog'></i> settings</a></li> -->
+  </ul>
 </div>
 
   <!-- Per cambiare le icone vai su https://boxicons.com e prendi la sezione FONT -->
@@ -37,10 +45,9 @@
     <li><a href="whoweare.html"><i class='bx bx-group' ></i> who we are</a></li>
     <li><a href="whatwedo.html"><i class='bx bx-briefcase'></i> what we do</a></li>
     <li><a href="help.html"><i class='bx bx-help-circle' ></i> help</a></li>
-    <li><a href="contact.html"><i class='bx bxs-contact'></i> contact</a></li>
-    <li><a href="credits.html"><i class='bx bxl-creative-commons'></i> credit</a></li>
-    </ul>
-  </div>
+    <li><a href="contacts.html"><i class='bx bxs-contact'></i> contact</a></li>
+  </ul>
+</div>
 
 
   <!-- Barra di Navigazione -->
@@ -50,8 +57,8 @@
           <h2>EduStream - Area Riservata</h2>
         </div>
         <div class="links">
-          <a href="#">TEXT</a>
-          <a href="#">TEXT</a>
+          <!--<a href="#">TEXT</a>
+          <a href="#">TEXT</a>-->
           <div class="dropdown">
             <a href="#">
               <div class="pfp">
@@ -61,11 +68,8 @@
               <img src="img/chevron.png"/>
             </a>
             <div class="menu">
-              <a href="#"> opzione1 </a>
-              <a href="#"> opzione2 </a>
-              <a href="#"> opzione3 </a>
-              <a href="#"> opzione4 </a>
-              <a href="#"> opzione5 </a>
+              <a href="php/account-details.php"> Account details </a>
+              <a href="#"> Settings </a>
               <a href="php/logout.php" class="login-link"> logout </a>
             </div>
           </div>
